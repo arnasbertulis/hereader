cb80f19284a625377dd7636af98d75577c66f192
Arnas Bertulis <118692829+arnasbertulis@users.noreply.github.com>
2026-09-14 18:51:25 +0300

    fix(app): sort visible_stream_builder_test.dart's directives
    flutter analyze's directives_ordering lint failed CI: package:app/ must
    sort before package:flutter_test/.

--- app/test/visible_stream_builder_test.dart
+++ app/test/visible_stream_builder_test.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
+import 'package:app/reading/visible_stream_builder.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:app/reading/visible_stream_builder.dart';
 
 void main() {
   late StreamController<int> controller;
